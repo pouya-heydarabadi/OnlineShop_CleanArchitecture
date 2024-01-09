55fb954b87629da6932b9b1954ff8026afa8e848
pouya heydarabadi <mry49hi@gmail.com>
2024-01-09 23:42:49 +0330

    feat: Add Main Stracture Value Object

---
+++ Domain/OnlineShop.Domain/Users/ValueObjests/FirstName.cs
@@ -0,0 +1,29 @@
+﻿using OnlineShop.Domain.Users.Exceptions.ValueObject.FirstName;
+
+namespace OnlineShop.Domain.Users.ValueObjests;
+
+public class FirstName
+{
+    #region Fields
+    public string Value { get; init; }
+    #endregion
+
+    #region Ctors
+    private FirstName(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new FirstNameCannotBeEmptyException();
+        if (value.Length <= 2)
+            throw new FirstNameCannotBeLessThanTwoCharException();
+
+        this.Value = value;
+    }
+
+
+    #region Factories
+    public static FirstName FromString(string value)
+        => new FirstName(value);
+    #endregion
+
+    #endregion
+}
