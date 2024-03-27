c37847b0c97636bdaa408b3f07705ceaa96eab2d
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-27 15:37:05 +0330

    feat: Add LastName Exceptions

---
+++ Domain/OnlineShop.Domain/Users/Exceptions/ValueObject/LastName/LastNameBadFormatException.cs
@@ -0,0 +1,10 @@
+﻿using OnlineShop.Shared.Exceptions;
+
+namespace OnlineShop.Domain.Users.Exceptions.ValueObject.LastName;
+
+public class LastNameBadFormatException : OnlineShopException
+{
+    public LastNameBadFormatException(string message, string newValue) : base($"{message} -- Value Is {newValue}")
+    {
+    }
+}
