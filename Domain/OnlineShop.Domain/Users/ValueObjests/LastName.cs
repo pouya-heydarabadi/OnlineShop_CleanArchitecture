d52bb5f1e8d980b9b18fdd19da370436f0833747
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-27 15:34:58 +0330

    feat: Add LastName ValueObject

---
+++ Domain/OnlineShop.Domain/Users/ValueObjests/LastName.cs
@@ -0,0 +1,20 @@
+﻿using OnlineShop.Domain.Users.Exceptions.ValueObject.LastName;
+
+namespace OnlineShop.Domain.Users.ValueObjests;
+
+public record LastName
+{
+    public string value { get; }
+
+    private LastName(string newValue)
+    {
+        this.value = newValue;
+    }
+
+    public static LastName Create(string newValue)
+    {
+        if(newValue.Length < 15)
+            throw new LastNameBadFormatException("Your Value Is Bad Format Because LastName Cannot Be Less Than 15 Character Lenght", newValue)
+        return new LastName(newValue);
+    }
+}
