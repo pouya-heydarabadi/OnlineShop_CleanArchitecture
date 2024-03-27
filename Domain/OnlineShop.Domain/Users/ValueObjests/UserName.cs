d4a960409407014c7c4c4bdd8fda9ef50d63922f
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-27 15:35:10 +0330

    feat: Add UserName ValueObject

---
+++ Domain/OnlineShop.Domain/Users/ValueObjests/UserName.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+using OnlineShop.Domain.Users.Exceptions.ValueObject.UserNames;
+
+namespace OnlineShop.Domain.Users.ValueObjests;
+
+public sealed record UserName
+{
+    public string value { get; } = "";
+
+    // For Base
+    private UserName(string newUserName)
+    {
+        this.value = newUserName;
+    }
+
+    public static UserName Create(string newUserName)
+    {
+        if (newUserName.Length < 3)
+            throw new UserNameCannotBeLessThanThreeCharacterException(userName: newUserName);
+
+        if (int.TryParse(newUserName, out int userNameInt))
+            throw new UserNameCannotBeEnterIntegerException(userNameInt);
+
+        return new UserName(newUserName);
+    }
+}
