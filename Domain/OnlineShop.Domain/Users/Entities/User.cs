0eb4c23ab2c713f4652caa4eba95c4445e3af26c
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-27 15:37:25 +0330

    feat: Add LastName And UserName ValueObject To User Entity

--- Domain/OnlineShop.Domain/Users/Entities/User.cs
+++ Domain/OnlineShop.Domain/Users/Entities/User.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineShop.Domain.Users.ValueObjests;
+
 using System.Security.Cryptography.X509Certificates;
 
 namespace OnlineShop.Domain.Users.Entities;
@@ -6,20 +7,23 @@ namespace OnlineShop.Domain.Users.Entities;
 public class User
 {
     public Guid Id { get; set; }
-    public string UserName { get; init; }
+    public UserName UserName { get; init; }
     public FirstName FirstName { get; init; }
-    public string LastName { get; init; }
+    public LastName LastName { get; init; }
     public string Email { get; init; }
     public string Password { get; init; }
 
     #region Ctors
+    #region Main Ctors
     //For EF
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     private User()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
-        
+
     }
 
-    private User(string userName, FirstName firstName, string lastName, string email, string passWord)
+    private User(UserName userName, FirstName firstName, string lastName, string email, string passWord)
     {
         this.UserName = userName;
         this.FirstName = firstName;
@@ -27,13 +31,30 @@ public class User
         this.Email = email;
         this.Password = passWord;
     }
+    #endregion
 
     #region Factories
-    public static User CreateUserWithAutoPassword(string userName, FirstName firstName, string lastName, string email)
-        => new User(userName: userName, firstName: firstName, lastName: lastName, email: email, passWord: Guid.NewGuid().ToString());
+    public static User CreateUserWithAutoPassword(string userName,
+                                                  FirstName firstName,
+                                                  string lastName,
+                                                  string email)
+        => new User
+            (userName: userName,
+             firstName: firstName,
+             lastName: lastName,
+             email: email,
+             passWord: Guid.NewGuid().ToString());
 
-    public static User Main(string userName, FirstName firstName, string lastName, string email, string passWord)
-        => new User(userName: userName, firstName: firstName, lastName: lastName, email: email, passWord: passWord);
+    public static User Main(string userName,
+                            FirstName firstName,
+                            string lastName,
+                            string email,
+                            string passWord)
+        => new User(userName: userName,
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    passWord: passWord);
     #endregion
 
     #endregion
