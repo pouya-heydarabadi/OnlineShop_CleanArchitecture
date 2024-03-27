506c7f0f3f67d0dd06d9d1b3e28a79111f8be936
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-27 15:36:23 +0330

    feat: Add FirstName Exceptions

---
+++ Domain/OnlineShop.Domain/Users/Exceptions/ValueObject/FirstNames/FirstNameCannotBeEmptyException.cs
@@ -0,0 +1,10 @@
+﻿using OnlineShop.Shared.Exceptions;
+
+namespace OnlineShop.Domain.Users.Exceptions.ValueObject.FirstNames;
+
+public class FirstNameCannotBeEmptyException : OnlineShopException
+{
+    public FirstNameCannotBeEmptyException() : base("FirstName Cannot Be Empty Value")
+    {
+    }
+}
