76fb903e92676bf9ed385fec08b76317a1ccbce1
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-27 15:36:10 +0330

    feat: Add UserName Exceptions

---
+++ Domain/OnlineShop.Domain/Users/Exceptions/ValueObject/UserNames/UserNameCannotBeEnterIntegerException.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShop.Shared.Exceptions;
+
+namespace OnlineShop.Domain.Users.Exceptions.ValueObject.UserNames;
+
+public class UserNameCannotBeEnterIntegerException : OnlineShopException
+{
+    public UserNameCannotBeEnterIntegerException(int userNameIntType)
+        : base($"You Cant Enter Number To UserName -- UserName is {userNameIntType}")
+    {
+    }
+}
---
+++ Domain/OnlineShop.Domain/Users/Exceptions/ValueObject/UserNames/UserNameCannotBeLessThanThreeCharacterException.cs
@@ -0,0 +1,13 @@
+﻿using OnlineShop.Domain.Users.ValueObjests;
+using OnlineShop.Shared.Exceptions;
+
+namespace OnlineShop.Domain.Users.Exceptions.ValueObject.UserNames;
+
+public class UserNameCannotBeLessThanThreeCharacterException : OnlineShopException
+{
+    public UserNameCannotBeLessThanThreeCharacterException(string userName)
+        : base($"Your UserName Cannot Be Less Than To Three Character {userName} -- Lenght is {userName.Length}")
+    {
+
+    }
+}
